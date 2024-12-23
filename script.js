6a82ffaeb9037caffd8f9582c90760549ab47ba5
Leonardo Goncalves <odranoeltsu2016@gmail.com>
2024-12-23 19:02:59 -0300

    Criação da secão de habilidades
    Criação de animações e modal sobre os tipos de tecnologias que uso

---
+++ script.js
@@ -0,0 +1,14 @@
+function openModal(modalId, fadeId){
+    const modal = document.getElementById(modalId)
+    const fade = document.getElementById(fadeId)
+    modal.classList.remove('fadeOut');
+    modal.classList.remove('hide')
+    fade.classList.remove('hide')
+}
+
+function closeModal(modalId, fadeId){
+    const modal = document.getElementById(modalId)
+    const fade = document.getElementById(fadeId)
+    modal.classList.add('hide');
+    fade.classList.add('hide')
+}
